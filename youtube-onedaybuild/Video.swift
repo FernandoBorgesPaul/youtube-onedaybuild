6d2bd15f69ac963502b2f6340b0e5ce8e4c8c0b2
Fernando Borges Paul <fernandoborgespaul@MacBook-Pro-de-Fernando.local>
2021-01-07 15:48:19 -0600

    Added model and networking setup to connect with Youtube API

---
+++ youtube-onedaybuild/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  youtube-onedaybuild
+//
+//  Created by Fernando Borges Paul on 07/01/21.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+}
