ee1ef001ceddfd58c45a05bdadcc884e8149287b
Fernando Borges Paul <fernandoborgespaul@MacBook-Pro-de-Fernando.local>
2021-01-11 10:46:23 -0600

    Adding Constants.swift file to tets bitrise

---
+++ youtube-onedaybuild/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  youtube-onedaybuild
+//
+//  Created by Fernando Borges Paul on 07/01/21.
+//
+
+import Foundation
+
+
+struct  K {
+  static var API_KEY = ""
+  static var PLAYLIST_ID = "RDOXkhZ8IxFSQ"
+  static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(K.PLAYLIST_ID)&key=\(K.API_KEY)"
+  static var VIDEOCELL_ID = "VideoCell"
+    
+}
